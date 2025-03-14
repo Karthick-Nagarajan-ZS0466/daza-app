0340810a6056c38a941fe4dded5cb7a7a47652c7
Karthick Nagarajan <karthick.nagarajan@zucisystems.com>
2025-03-14 13:14:46 +0530

    Added Angular Material and Store Packages

--- daza/src/app/app.component.ts
+++ daza/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- daza/src/app/app.config.server.ts
+++ daza/src/app/app.config.server.ts
@@ -3,9 +3,20 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideRouter } from '@angular/router';
+import { StateObservable, Store } from '@ngrx/store';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
+  providers: [
+    provideServerRendering(),
+    provideServerRouting(serverRoutes),
+    Store,
+    {
+      provide: StateObservable,
+      useFactory: (store: Store) => store,
+      deps: [Store],
+    },
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
